f827d1c45f26956449ab2d0fad70b25d1d14b4e6
Ibrahim Mubarok <mubarokibrahim@gmail.com>
2022-10-05 12:45:55 +0700

    add some dependencies and some themes

---
+++ lib/utils/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xFF007665);
+Color backgroundColor = const Color(0xFFF7FFF7);
+Color greyColor = const Color(0xFFC5BDBD);
+Color greyTwoColor = const Color(0xFFB1B1B1);
+Color greyThreeColor = const Color(0xFFE4E4E4);
+Color greyFourColor = const Color(0xFF9C9797);
+Color greenColor = const Color(0xFF007665);
+Color redColor = const Color(0xFFFF7B7B);
+Color yellowColor = const Color(0xFFF4BD46);
+Color greenTwoColor = const Color(0xFF55A99D);
+Color greenThreeColor = const Color(0xFFD0ECE8);
+Color greenAccentColor = const Color(0xFFAFFFC6);
+Color blackColor = const Color(0xFF000000);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color backgroundCreamColor = const Color(0xFFFFA070);
+Color backgroundBlueColor = const Color(0xFF25CBFF);
+Color transparentColor = Colors.transparent;
+
+///Font Weight
+FontWeight light = FontWeight.w300;
+FontWeight normal = FontWeight.w400;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+///Text Styling
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle greyTwoTextStyle = GoogleFonts.poppins(color: greyTwoColor);
+TextStyle greyThreeTextStyle = GoogleFonts.poppins(color: greyThreeColor);
+TextStyle redTextStyle = GoogleFonts.poppins(color: redColor);
+TextStyle yellowTextStyle = GoogleFonts.poppins(color: yellowColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
